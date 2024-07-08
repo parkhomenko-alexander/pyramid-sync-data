cb1942b2bb63e449af823672dddac3e29fa78ef5
parkhomenko-alexander <parkhomenko.alx@gmail.com>
2024-07-08 10:46:33 +1000

    change signature of sync function

--- app/celery/celery_app.py
+++ app/celery/celery_app.py
@@ -9,8 +9,8 @@ celery_app = Celery(
     backend=config.CELERY_RESULT_BACKEND,
     include=[
         "tasks.building.tasks",
-        "tasks.tag.tasks",
         "tasks.data.tasks",
+        "tasks.tag.tasks",
     ],
     broker_connection_retry=True,
     broker_connection_retry_on_startup=True,
@@ -20,6 +20,6 @@ celery_app.conf.beat_schedule = {
     "sync_history_data_every_2_hours": {
         "task": "schedule_sync_history_data",
         "schedule": crontab(minute="0", hour="*/2"),
-        "kwargs": {"tag_title": "EnergyActiveForward30Min", "hours_delta": config.ENERGY_SCHEDULE_TIME_DELTA, "time_partition": "30m", "meter_points": []}
+        "kwargs": {"tag_title": "EnergyActiveForward30Min", "days_delta": 10, "hours_delta": config.ENERGY_SCHEDULE_TIME_DELTA, "time_partition": "30m", "meter_points": []}
     }
 }
