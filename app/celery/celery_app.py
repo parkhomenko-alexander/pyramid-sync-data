7e9f0675bc2ad6f02b9a98f13333bd5e2c2ffb69
parkhomenko-alexander <parkhomenko.alx@gmail.com>
2025-07-24 02:29:02 +0000

    fix celery

--- app/celery/celery_app.py
+++ app/celery/celery_app.py
@@ -1,18 +1,20 @@
-from datetime import date, timedelta
-
 from celery import Celery
 from celery.schedules import crontab
 
 from config import config
+
+from loguru import logger
+
+logger.info(["Init celery", config,  config.CELERY_BROKER_URL],)
 
 celery_app = Celery(
     __name__,
     broker=config.CELERY_BROKER_URL,
     backend=config.CELERY_RESULT_BACKEND,
     include=[
-        "tasks.building.tasks",
-        "tasks.data.tasks",
-        "tasks.tag.tasks",
+        "app.celery.tasks.building.tasks",
+        "app.celery.tasks.data.tasks",
+        "app.celery.tasks.tag.tasks",
     ],
     broker_connection_retry=True,
     broker_connection_retry_on_startup=True,
@@ -20,7 +22,7 @@ celery_app = Celery(
 
 celery_app.conf.beat_schedule = {
     "sync_history_data_every_2_hours": {
-        "task": "tasks.data.tasks.schedule_sync_history_data",
+        "task": "app.celery.tasks.data.tasks.schedule_sync_history_data",
         "schedule": crontab(minute="*/40"),
         "kwargs": {"tag_title": "EnergyActiveForward30Min", "time_partition": "1month",}
     }
--- config.py
+++ config.py
@@ -2,39 +2,38 @@ import os
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-DOTENV = os.path.join(os.path.dirname(__file__), ".env")
 
 class Config(BaseSettings):
-    CELERY_BROKER_URL: str = ""
-    CELERY_RESULT_BACKEND: str = ""
+    CELERY_BROKER_URL: str
+    CELERY_RESULT_BACKEND: str
 
-    API_BASE_URL: str = ""
-    API_USER: str = ""
-    API_PAS: str = ""
-    API_SOAP_BASE_URL: str = ""
-    API_SOAP_USER: str = ""
-    API_SOAP_PAS: str = ""
+    API_BASE_URL: str
+    API_USER: str
+    API_PAS: str
+    API_SOAP_BASE_URL: str
+    API_SOAP_USER: str
+    API_SOAP_PAS: str
     API_CALLS_DELAY: float = 0
     API_CALLS_DELAY_TIMEOUT_ERROR: float = 0
-    APP_DATA_DIR: str = ""
+    APP_DATA_DIR: str
  
-    DB_URI: str = ""
+    DB_URI: str
     DB_ECHO: bool = False
 
     APPLICATION_PREFIX_BEHIND_PROXY: str
-    APPLICATION_LOGGER_PATH: str = ""
-    APPLICATION_LOGGER_FILENAME: str = ""
-    APPLICATION_LOGGER_FORMAT: str = ""
-    APPLICATION_LOGGER_ROTATION: str = ""
-    APPLICATION_LOGGER_COMPRESSION: str = ""
+    APPLICATION_LOGGER_PATH: str
+    APPLICATION_LOGGER_FILENAME: str
+    APPLICATION_LOGGER_FORMAT: str
+    APPLICATION_LOGGER_ROTATION: str
+    APPLICATION_LOGGER_COMPRESSION: str
     APPLICATION_LOGGER_SERIALIZE: bool = True
 
-    POSTGRES_DB: str = ""
-    POSTGRES_PASSWORD: str = ""
-    POSTGRES_USER: str = ""
+    POSTGRES_DB: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_USER: str
 
     ENERGY_SCHEDULE_TIME_DELTA: int = 1
       
-    model_config = SettingsConfigDict(env_file=DOTENV)
+    model_config = SettingsConfigDict(env_file=".env")
 
 config = Config() # type: ignore
